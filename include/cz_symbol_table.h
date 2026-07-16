4d2d350be3c4a6eb1819d56aee6af73d8ba3fd40
김주현 <paulkim001@autocrypt.io>
2026-07-16 15:50:57 +0900

    Symbol table

--- include/cz_symbol_table.h
+++ include/cz_symbol_table.h
@@ -48,6 +48,7 @@ struct CZ_Environment {
 
     CZ_Symbol** symbols;
     unsigned int symbol_count;
+    unsigned int symbol_capacity;
 
     unsigned int scope_level;
 };
@@ -67,6 +68,29 @@ CZ_Symbol* cz_symbol_create(CZ_SymbolKind kind, const char* name);
  * @param symbol Pointer to CZ_Symbol
  */
 void cz_symbol_free(CZ_Symbol* symbol);
+
+/**
+ * @brief Create CZ_Environment
+ * 
+ * @return CZ_Environment* Pointer to CZ_Environment on success, NULL on failure.
+ */
+CZ_Environment* cz_environment_create(void);
+
+/**
+ * @brief Frees environment
+ * 
+ * @param env Pointer to CZ_Environment
+ */
+void cz_environment_free(CZ_Environment* env);
+
+/**
+ * @brief Pushes symbol to environment.
+ * 
+ * @param env Pointer to CZ_Environment
+ * @param symbol Pointer to CZ_Symbol. (Do not free, as ownership is being transferred)
+ * @return int 1 on success, 0 on failure.
+ */
+int cz_environment_push_symbol(CZ_Environment* env, const CZ_Symbol* symbol);
 
 #ifdef __cplusplus
 }
--- src/cz_symbol_table.c
+++ src/cz_symbol_table.c
@@ -53,3 +53,61 @@ void cz_symbol_free(CZ_Symbol* symbol) {
     }
     free(symbol);
 }
+
+CZ_Environment* cz_environment_create(void) {
+    CZ_Environment* env = NULL;
+    CZ_Symbol** symbols = NULL;
+
+    env = (CZ_Environment*) calloc(1, sizeof(CZ_Environment));
+    NULL_POINTER_TO_GOTO(env, error_cleanup);
+
+    env->symbol_capacity = 8;
+    symbols = (CZ_Symbol**) calloc(env->symbol_capacity, sizeof(CZ_Symbol*));
+    NULL_POINTER_TO_GOTO(symbols, error_cleanup);
+
+    env->symbols = symbols;
+    symbols = NULL;
+
+    return env;
+
+error_cleanup:
+    free(env);
+    free(symbols);
+    return NULL;
+}
+
+void cz_environment_free(CZ_Environment* env) {
+    if (env != NULL) {
+        if (env->symbols != NULL) {
+            for (unsigned int i = 0; i < env->symbol_count; i++) {
+                cz_symbol_free(env->symbols[i]);
+                env->symbols[i] = NULL;
+            }
+        }
+        free(env->symbols);
+    }
+    free(env);
+}
+
+int cz_environment_push_symbol(CZ_Environment* env, const CZ_Symbol* symbol) {
+    CZ_Symbol** new_symbols = NULL;
+    NULL_POINTER_TO_GOTO(env, error_cleanup);
+    NULL_POINTER_TO_GOTO(symbol, error_cleanup);
+
+    if (env->symbol_capacity == env->symbol_count) {
+        new_symbols = (CZ_Symbol**) realloc(env->symbols, sizeof(CZ_Symbol*) * env->symbol_capacity * 2);
+        NULL_POINTER_TO_GOTO(new_symbols, error_cleanup);
+
+        env->symbols = new_symbols;
+        new_symbols = NULL;
+        env->symbol_capacity *= 2;
+    }
+    env->symbols[env->symbol_count] = (CZ_Symbol*) symbol;
+    env->symbol_count++;
+
+    return 1;
+
+error_cleanup:
+    free(new_symbols);
+    return 0;
+}
